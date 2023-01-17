ec2352fb92784258d16c9bf64e1f9a4618d2ad83
Grazia Ferrara <g.ferrara75@studenti.unisa.it>
2023-01-17 02:13:21 +0100

    tarjan ok

--- measures.sh
+++ measures.sh
@@ -15,73 +15,76 @@ trap "exit" INT
 # Get script path
 SCRIPTPATH=$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
 
-# for shrink in "${ARRAY_SHRINK[@]}"; do
+for shrink in "${ARRAY_SHRINK[@]}"; do
 
-# 	for size in "${ARRAY_RC[@]}"; do
+	for size in "${ARRAY_RC[@]}"; do
 
-# 		for ths in "${ARRAY_THS[@]}"; do
-# 			ths_str=$(printf "%02d" $ths)
+		for ths in "${ARRAY_THS[@]}"; do
+			ths_str=$(printf "%02d" $ths)
 
-# 			OUT_FILE=$SCRIPTPATH/Measures/Tarjan/SIZE-$size-E-$shrink/SIZE-$size-NP-$ths_str-E-$shrink.csv
-# 			# OUT_FILE2=$SCRIPTPATH/Measures/Kosaraju/SIZE-$size-E-$shrink/SIZE-$size-NP-$ths_str-E-$shrink.csv
+			OUT_FILE=$SCRIPTPATH/Measures/Tarjan/SIZE-$size-E-$shrink/SIZE-$size-NP-$ths_str-E-$shrink.csv
+			# OUT_FILE2=$SCRIPTPATH/Measures/Kosaraju/SIZE-$size-E-$shrink/SIZE-$size-NP-$ths_str-E-$shrink.csv
 
-# 			mkdir -p $(dirname $OUT_FILE) 2> /dev/null
-# 			# mkdir -p $(dirname $OUT_FILE2) 2> /dev/null
+			mkdir -p $(dirname $OUT_FILE) 2> /dev/null
+			# mkdir -p $(dirname $OUT_FILE2) 2> /dev/null
 
-# 			echo $(basename $OUT_FILE)
+			echo $(basename $OUT_FILE)
 
-# 				if [[ $ver -gt 1 && $ths -eq 0 ]]; then
-# 					OLD_OUT_FILE=$SCRIPTPATH/Measures/Tarjan/SIZE-$size-E-$shrink/SIZE-$size-NP-$ths_str-E-$shrink.csv
-# 					ln -srf -T $OLD_OUT_FILE $OUT_FILE
-# 					echo Created symbolic link to $(basename $OLD_OUT_FILE)
-# 					# OLD_OUT_FILE2=$SCRIPTPATH/Measures/Kosaraju/SIZE-$size-E-$shrink/SIZE-$size-NP-$ths_str-E-$shrink.csv
-# 					# ln -srf -T $OLD_OUT_FILE2 $OUT_FILE2
-# 					# echo Created symbolic link to $(basename $OLD_OUT_FILE2)
-# 					continue
-# 				fi
+				if [[ $ver -gt 1 && $ths -eq 0 ]]; then
+					OLD_OUT_FILE=$SCRIPTPATH/Measures/Tarjan/SIZE-$size-E-$shrink/SIZE-$size-NP-$ths_str-E-$shrink.csv
+					ln -srf -T $OLD_OUT_FILE $OUT_FILE
+					echo Created symbolic link to $(basename $OLD_OUT_FILE)
+					# OLD_OUT_FILE2=$SCRIPTPATH/Measures/Kosaraju/SIZE-$size-E-$shrink/SIZE-$size-NP-$ths_str-E-$shrink.csv
+					# ln -srf -T $OLD_OUT_FILE2 $OUT_FILE2
+					# echo Created symbolic link to $(basename $OLD_OUT_FILE2)
+					continue
+				fi
 
-# 			echo "size,processes,read,SCC,write,elapsed" >$OUT_FILE
-# 			# echo "size,processes,read,SCC,write,elapsed" >$OUT_FILE2
+			echo "size,processes,read,SCC,write,elapsed" >$OUT_FILE
+			# echo "size,processes,read,SCC,write,elapsed" >$OUT_FILE2
 
-# 			for ((i = 0 ; i < $NMEASURES; i++)); do
+			for ((i = 0 ; i < $NMEASURES; i++)); do
 				
-# 				if [[ $ths -eq 0 ]]; then
-# 					# serial
+				if [[ $ths -eq 0 ]]; then
+					# serial
 
-# 					./Source/updateConstants.sh $size $(($size/$shrink)) $(($size/$shrink))
-# 					make -B
+					./Source/updateConstants.sh $(($size/$MAX_PROC)) $(($size/$shrink)) $(($size/$shrink))
+					make -B
 
-# 					# write graphs
-# 					mpirun -np 1 ./Build/wg.o
-# 					# read graphs and write on file th whole matrix
-# 					mpirun -np 1 ./Build/rg.o
+					# write graphs
+					mpirun -np $MAX_PROC ./Build/wg.o
+					# read graphs and write on file th whole matrix
+					mpirun -np $MAX_PROC ./Build/rg.o
 
-# 					#execute serial
-# 					echo $size,$ths,$(./Build/s.o) >> $OUT_FILE
-# 					# echo $size,$ths,$(./Build/s_k.o) >> $OUT_FILE2
-# 					printf "\r> %d/%d %3.1d%% " $(expr $i + 1) $NMEASURES $(expr \( \( $i + 1 \) \* 100 \) / $NMEASURES)
-# 				else
-# 					# parallel
-# 					new=$(($size/$ths))
-# 					./Source/updateConstants.sh $new $(($size/$shrink)) $(($size/$shrink))
-# 					make -B
+					./Source/updateConstants.sh $size $(($size/$shrink)) $(($size/$shrink))
+					make -B
 
-# 					# write graphs
-# 					mpirun -np $ths ./Build/wg.o
+					#execute serial
+					echo $size,$ths,$(./Build/s.o) >> $OUT_FILE
+					# echo $size,$ths,$(./Build/s_k.o) >> $OUT_FILE2
+					printf "\r> %d/%d %3.1d%% " $(expr $i + 1) $NMEASURES $(expr \( \( $i + 1 \) \* 100 \) / $NMEASURES)
+				else
+					# parallel
+					new=$(($size/$ths))
+					./Source/updateConstants.sh $new $(($size/$shrink)) $(($size/$shrink))
+					make -B
+
+					# write graphs
+					mpirun -np $ths ./Build/wg.o
 					
-# 					#execute parallel
-# 					echo $size,$ths,$(mpirun -np $ths ./Build/p.o) >> $OUT_FILE
-# 					# echo $size,$ths,$(mpirun -np $ths ./Build/p_k.o) >> $OUT_FILE2
-# 					printf "\r> %d/%d %3.1d%% " $(expr $i + 1) $NMEASURES $(expr \( \( $i + 1 \) \* 100 \) / $NMEASURES)
-# 				fi
+					#execute parallel
+					echo $size,$ths,$(mpirun -np $ths ./Build/p.o) >> $OUT_FILE
+					# echo $size,$ths,$(mpirun -np $ths ./Build/p_k.o) >> $OUT_FILE2
+					printf "\r> %d/%d %3.1d%% " $(expr $i + 1) $NMEASURES $(expr \( \( $i + 1 \) \* 100 \) / $NMEASURES)
+				fi
 
-# 			done
+			done
 
-# 			printf "\n"
+			printf "\n"
 			
-# 		done
-# 	done
-# done
+		done
+	done
+done
 
 # we choose a size on which test the optimization levels
 TEST_SIZE=400
@@ -123,13 +126,16 @@ for level in "${ARRAY_OPT[@]}"; do
 			if [[ $ths -eq 0 ]]; then
 				# serial
 
-				./Source/updateConstants.sh $TEST_SIZE $(($TEST_SIZE/$SHRINK)) $(($TEST_SIZE/$SHRINK))
+				./Source/updateConstants.sh $(($TEST_SIZE/$MAX_PROC)) $(($TEST_SIZE/$SHRINK)) $(($TEST_SIZE/$SHRINK))
 				make $opt -B
 
 				# write graphs
-				mpirun -np 1 ./Build/wg.o
+				mpirun -np $MAX_PROC ./Build/wg.o
 				# read graphs and write on file th whole matrix
-				mpirun -np 1 ./Build/rg.o
+				mpirun -np $MAX_PROC ./Build/rg.o
+
+				./Source/updateConstants.sh $TEST_SIZE $(($TEST_SIZE/$SHRINK)) $(($TEST_SIZE/$SHRINK))
+				make $opt -B
 
 				#execute serial
 				echo $TEST_SIZE,$ths,$(./Build/s.o) >> $OUT_FILE
